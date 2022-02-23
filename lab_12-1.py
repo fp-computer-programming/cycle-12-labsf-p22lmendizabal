5789d41bebb3823c66ae10ac2e829b6d149c73e3
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-23 10:15:28 -0500

    add lab 12 files

--- lab_12-1.py
+++ lab_12-1.py
@@ -1,7 +1,8 @@
 # author: LM (AMDG) 2/15/22
-from fileinput import close
 
-
+#opens file
 ofile = open('my_file.txt', "w")
+#writes a new line 
 ofile.write('LM 2/15/22 \n Hello World \n I have been playing soccer')
+#closes file
 ofile.close()
--- lab_12-2.py
+++ lab_12-2.py
@@ -1,3 +1,6 @@
+#opnes a file
 openfile = open("my_file.txt", "a")
+#appending a new line
 openfile.write(" \n I am using social media to stay connected \n I will meet up with friends")
+#closes file
 openfile.close()
